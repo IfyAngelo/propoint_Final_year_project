40fce55d7f4edc5be223f1f1ea73ce594799858e
Bolorunduro Valiant-Joshua <valiantjoshuabolorunduro@gmail.com>
2023-03-11 23:51:16 +0100

    chore: Add health check on base route

--- appcopy.py
+++ appcopy.py
@@ -6,21 +6,30 @@ from transformers import BertTokenizer, BertForSequenceClassification
 tokenizer = BertTokenizer.from_pretrained('DataAngelo/propoint_Final_project')
 tokenizer.save_pretrained("./tokenizers")
 
-model = BertForSequenceClassification.from_pretrained('DataAngelo/propoint_Final_project')
+model = BertForSequenceClassification.from_pretrained(
+    'DataAngelo/propoint_Final_project')
 model.save_pretrained("./propoint_model")
 
 app = Flask(__name__)
 
+
+@app.route('/', methods=['GET'])
+def health():
+    return jsonify({'status': "App is healthy"})
+
+
 @app.route('/predict', methods=['POST'])
 def propoint_predict():
     query = request.json['query']
-    input = tokenizer.encode_plus(query, add_special_tokens=True, return_tensors='pt')
+    input = tokenizer.encode_plus(
+        query, add_special_tokens=True, return_tensors='pt')
 
     output = model(**input)
     logits = output[0]
     predicted_label = torch.argmax(logits, dim=1).tolist()[0]
 
-    return jsonify({'label':predicted_label})
+    return jsonify({'label': predicted_label})
+
 
 # run flask app
 if __name__ == '__main__':
