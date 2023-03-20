e6844c2d5b67c7f2412138e6d2c867091a1bbfbb
Michaelangelo <michaelangeloifeanyichukwu@gmail.com>
2023-03-20 19:42:04 +0100

    updated

--- appcopy.py
+++ appcopy.py
@@ -30,7 +30,7 @@ def propoint_predict():
     label_map = {0: 'Poor', 1: 'Bad', 2: 'Average', 3: 'Good', 4: 'Very Good', 5: 'Excellent'}
     predicted_label_name = label_map[predicted_label]
 
-    return jsonify({'label':predicted_label})
+    return jsonify({'label':predicted_label, 'label_name' :predicted_label_name})
 
 # run flask app
 if __name__ == '__main__':
